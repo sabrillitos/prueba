e095b26a7412dfbb74a9715b035c90f4138d848d
sabrillitos <118294413+sabrillitos@users.noreply.github.com>
2025-11-20 00:57:58 -0300

    Delete AppAlumnos directory

--- AppAlumnos/src/models/SerializableCSV.java
+++
@@ -1,9 +0,0 @@
-package models;
-
-
-public interface SerializableCSV {
-    
-    String toCSV(); // no hace falta poner public, la interface ya es PUBLICA POR DEFECTO
-
-    SerializableCSV fromCSV(String linea);
-}
